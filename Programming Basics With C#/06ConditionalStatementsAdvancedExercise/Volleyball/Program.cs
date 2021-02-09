aa2b19b7a90433e50128a6b37507c7538b0473c3
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-02-09 20:52:59 +0200

    adding a task

---
+++ Programming Basics With C#/06ConditionalStatementsAdvancedExercise/Volleyball/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfYear = Console.ReadLine();
+            double p = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double weekendsPerYear = 48.0;
+            double weekendsInSofia = 0.0;
+            double soturdayGamesSofia = 0.0;
+            double holidays = 0.6666666666666667;
+            double holidaysGames = 0.0;
+            double gamesInSofia = 0.0;
+            double moreGames = 1.0;
+            weekendsInSofia = weekendsPerYear - h;
+            soturdayGamesSofia = weekendsInSofia * 0.75;
+            holidaysGames = p * holidays;
+            gamesInSofia = soturdayGamesSofia + holidaysGames + h;
+            if (typeOfYear == "leap")
+            {
+                moreGames = 1.15;
+            }
+
+            Console.WriteLine(Math.Floor(gamesInSofia * moreGames));
+        }
+    }
+}
