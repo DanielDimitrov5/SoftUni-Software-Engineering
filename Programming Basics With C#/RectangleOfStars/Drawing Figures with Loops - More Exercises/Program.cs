c3e4016e3c88939d8f8781407fa5c939a00058cc
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-02-21 14:04:10 +0200

    add

---
+++ Programming Basics With C#/RectangleOfStars/Drawing Figures with Loops - More Exercises/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Drawing_Figures_with_Loops___More_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+                Console.WriteLine("*");
+                Environment.Exit(0);
+            }
+
+            int leftRight = (n - 1) / 2;
+            int middle = -1;
+            int middleB = 0;
+
+            if (n % 2 == 1)
+            {
+                Console.Write(new string('-', leftRight));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('-', leftRight));
+                Console.WriteLine();
+
+                for (int i = 0; i < ((n - 2) / 2) + 1; i++)
+                {
+                    Console.Write(new string('-', leftRight - 1));
+                    leftRight -= 1;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', middle + 2));
+                    middle += 2;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', leftRight));
+                    Console.WriteLine();
+                }
+
+                for (int bottom = 0; bottom < (n - 2) / 2; bottom++)
+                {
+                    Console.Write(new string('-', leftRight + 1));
+                    leftRight += 1;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', middle - 2));
+                    middle -= 2;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', leftRight));
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.Write(new string('-', leftRight));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('-', leftRight));
+                Console.WriteLine();
+
+                for (int i = 0; i < (n - 2) / 2; i++)
+                {
+                    Console.Write(new string('-', leftRight - 1));
+                    leftRight -= 1;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', middleB + 2));
+                    middleB += 2;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', leftRight));
+                    Console.WriteLine();
+                }
+
+                for (int i = 0; i < ((n - 2) / 2) - 1; i++)
+                {
+                    Console.Write(new string('-', leftRight + 1));
+                    leftRight += 1;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', middleB - 2));
+                    middleB -= 2;
+                    Console.Write(new string('*', 1));
+                    Console.Write(new string('-', leftRight));
+                    Console.WriteLine();
+                }
+            }
+
+            if (n % 2 == 1)
+            {
+                Console.Write(new string('-', (n - 1) / 2));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('-', (n - 1) / 2));
+                Console.WriteLine();
+            }
+            else if (n != 2)
+            {
+                Console.Write(new string('-', (n - 1) / 2));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('-', (n - 1) / 2));
+                Console.WriteLine();
+            }
+        }
+    }
+}
