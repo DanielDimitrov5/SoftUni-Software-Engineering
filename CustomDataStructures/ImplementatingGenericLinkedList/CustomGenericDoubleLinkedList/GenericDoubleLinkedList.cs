60d28457ca1224505d2547e6d17f2bf1ad377456
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-02-28 09:48:41 +0200

    Add GenericDoubleLinkedList

---
+++ CustomDataStructures/ImplementatingGenericLinkedList/CustomGenericDoubleLinkedList/GenericDoubleLinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoubleLinkedList
+{
+    class DoubleLinkedList<T>
+    {
+        private int count;
+        private bool reversed = false;
+
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+        
+        public void AddHead(Node<T> node)
+        {
+            count++;
+
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+            node.Next = Head;
+            Head.Previous = node;
+            Head = node;
+        }
+
+        public void AddTail(Node<T> node)
+
+        {
+            count++;
+
+            if (Tail == null)
+            {
+                Head = node;
+                Tail = node;
+                count++;
+                return;
+            }
+
+            node.Previous = Tail;
+            Tail.Next = node;
+            Tail = node;
+        }
+
+        public Node<T> RemoveHead()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+
+            Node<T> nodeToReturn = Head;
+
+            count--;
+
+            if (Head.Next != null)
+            {
+                Head = Head.Next;
+                Head.Previous = null;
+            }
+            else
+            {
+                Head = null;
+                Tail = null;
+            }
+
+            return nodeToReturn;
+        }
+
+        public Node<T> RemoveTail()
+        {
+            if (Tail == null)
+            {
+                return null;
+            }
+
+            Node<T> nodeToReturn = Tail;
+
+            count--;
+
+            if (Tail.Previous != null)
+            {
+                Tail = Tail.Previous;
+                Tail.Next = null;
+            }
+            else
+            {
+                Head = null;
+                Tail = null;
+            }
+
+            return nodeToReturn;
+        }
+
+        public void Foreach(Action<Node<T>> action)
+        {
+            Node<T> current = Head;
+
+            while (current != null)
+            {
+                action(current);
+
+                if (reversed)
+                {
+                    current = current.Previous;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[count];
+
+            int index = 0;
+
+            Foreach(x =>
+            {
+                array[index++] = x.Value;
+            });
+
+            return array;
+        }
+
+        //Reverse not fully functional
+        public void Reverse()
+        {
+            Node<T> temp = Head;
+            Head = Tail;
+            Tail = temp;
+
+            reversed = !reversed;
+        }
+    }
+}
---
+++ CustomDataStructures/ImplementatingGenericLinkedList/CustomGenericDoubleLinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDoubleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList<int> linkedList = new DoubleLinkedList<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                linkedList.AddHead(new Node<int>(i + 1));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                linkedList.AddTail(new Node<int>(i + 1));
+            }
+
+            linkedList.Foreach(x => Console.WriteLine(x.Value));
+
+            Console.WriteLine("--------------");
+
+            for (int i = 0; i < 3; i++)
+            {
+                linkedList.RemoveHead();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                linkedList.RemoveTail();
+            }
+
+            linkedList.Foreach(x => Console.WriteLine(x.Value));
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine(string.Join(" ", linkedList.ToArray()));
+
+            linkedList.Reverse(); //Reverse not fully functional
+
+            Console.WriteLine(string.Join(" ", linkedList.ToArray()));
+
+            var genericList = new DoubleLinkedList<string>();
+
+            genericList.AddHead(new Node<string>("Stefan"));
+            genericList.AddHead(new Node<string>("Gogi"));
+            genericList.AddHead(new Node<string>("Muci"));
+            genericList.AddHead(new Node<string>("Pesho"));
+
+            genericList.Foreach(x => Console.WriteLine(x.Value));
+
+            var nestedList = new DoubleLinkedList<List<char>>();
+
+            nestedList.AddHead(new Node<List<char>>(new List<char>() { 'a', 'b', 'v' }));
+            nestedList.AddTail(new Node<List<char>>(new List<char>() { 'g', 'd', 'e' }));
+
+            nestedList.Foreach(x => Console.WriteLine(string.Join(" ", x.Value)));
+        }
+    }
+}
