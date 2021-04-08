6529e21cf24c90a83bb19b42b530b343dd90c321
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-04-08 16:12:22 +0300

    add Iterators And Comparators - Lab

---
+++ C# Advanced/IteratorsAndComparatorsLab/Library/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparer<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int Compare(Book x, Book y)
+        {
+            int result = x.Title.CompareTo(y.Title);
+            if (result == 0)
+            {
+                result = x.Year.CompareTo(y.Year);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparatorsLab/Library/Library.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            //return new LibraryIterator(books);
+            foreach (var book in books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        //private class LibraryIterator : IEnumerator<Book>
+        //{
+        //    private readonly SortedSet<Book> books;
+        //    private int currentIndex;
+
+        //    public LibraryIterator(SortedSet<Book> books)
+        //    {
+        //        Reset();
+        //        this.books = books;
+        //    }
+
+        //    public Book Current => books[currentIndex];
+
+        //    object IEnumerator.Current => Current;
+
+        //    public void Dispose() { }
+
+        //    public bool MoveNext()
+        //    {
+        //        return ++currentIndex < books.Count;
+        //    }
+
+        //    public void Reset()
+        //    {
+        //        currentIndex = -1;
+        //    }
+        //}
+    }
+}
