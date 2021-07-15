9b0c8ea0bd1358cf220c55aba37f3c103ba00ced
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-07-15 13:37:19 +0300

    add Exam April 2021

---
+++ C# OOP/Exam Preparation/C# OOP Retake Exam – April 2021/Easter/Easter/Models/Bunnies/HappyBunny.cs
@@ -0,0 +1,11 @@
+﻿namespace Easter.Models.Bunnies
+{
+    public class HappyBunny : Bunny
+    {
+        private const int initialEnergy = 100;
+
+        public HappyBunny(string name) : base(name, initialEnergy)
+        {
+        }
+    }
+}
---
+++ C# OOP/Exam Preparation/C# OOP Retake Exam – April 2021/Easter/Easter/Repositories/EggRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+using System.Collections.Generic;
+using Easter.Models.Eggs.Contracts;
+using Easter.Repositories.Contracts;
+
+namespace Easter.Repositories
+{
+    public class EggRepository : IRepository<IEgg>
+    {
+        private List<IEgg> eggs;
+
+        public EggRepository()
+        {
+            eggs = new List<IEgg>();
+        }
+
+        public IReadOnlyCollection<IEgg> Models { get; private set; } = new List<IEgg>();
+
+        public void Add(IEgg model)
+        {
+            eggs.Add(model);
+
+            Models = eggs;
+        }
+
+        public IEgg FindByName(string name)
+        {
+            return eggs.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(IEgg model)
+        {
+            if (eggs.Contains(model))
+            {
+                eggs.Remove(model);
+
+                Models = eggs;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
