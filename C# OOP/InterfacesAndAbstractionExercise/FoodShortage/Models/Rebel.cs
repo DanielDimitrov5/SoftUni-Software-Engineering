5855676f9139bbe06c303900d58ee24725e1a48e
Daniel <dani_dimitrov2003@abv.bg>
2021-07-16 17:36:28 +0300

    Update Rebel.cs

--- C# OOP/InterfacesAndAbstractionExercise/FoodShortage/Models/Rebel.cs
+++ C# OOP/InterfacesAndAbstractionExercise/FoodShortage/Models/Rebel.cs
@@ -6,11 +6,6 @@ namespace FoodShortage.Models
 {
     public class Rebel : IBuyer
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Group { get; set; }
-        public int Food { get; set; }
-
         public Rebel(string name, int age, string group)
         {
             Name = name;
@@ -18,6 +13,11 @@ namespace FoodShortage.Models
             Group = group;
             Food = 0;
         }
+        
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Group { get; private set; }
+        public int Food { get; private set; }
 
         public void BuyFood()
         {
