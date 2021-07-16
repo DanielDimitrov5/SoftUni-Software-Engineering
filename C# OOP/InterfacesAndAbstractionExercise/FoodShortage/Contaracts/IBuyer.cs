e4a5fff97ea6929d4a8df6cc02850adae777b3c3
Daniel <dani_dimitrov2003@abv.bg>
2021-07-16 17:38:23 +0300

    Update IBuyer.cs

--- C# OOP/InterfacesAndAbstractionExercise/FoodShortage/Contaracts/IBuyer.cs
+++ C# OOP/InterfacesAndAbstractionExercise/FoodShortage/Contaracts/IBuyer.cs
@@ -2,8 +2,9 @@
 {
     public interface IBuyer
     {
-        public string Name { get; set; }
+        public string Name { get; }
         public int Food { get; }
+        
         void BuyFood();
     }
 }
