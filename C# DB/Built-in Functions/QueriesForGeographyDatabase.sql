ea8d70094a73590a61d55eaf02a3f0c9342d2432
Daniel <dani_dimitrov2003@abv.bg>
2021-08-04 19:25:44 +0300

    Update QueriesForGeographyDatabase.sql

--- C# DB/Built-in Functions/QueriesForGeographyDatabase.sql
+++ C# DB/Built-in Functions/QueriesForGeographyDatabase.sql
@@ -7,11 +7,11 @@ ORDER BY IsoCode
 SELECT PeakName, RiverName,
 	LOWER
 	(
-		CONCAT
-		(
-			SUBSTRING(PeakName, 1, LEN(PeakName) - 1), RiverName)
-		) 
-		AS Mix
+	  CONCAT
+	  (
+	      SUBSTRING(PeakName, 1, LEN(PeakName) - 1), RiverName)
+	  ) 
+	  AS Mix
 	FROM Peaks, Rivers
 	WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
 ORDER BY Mix
