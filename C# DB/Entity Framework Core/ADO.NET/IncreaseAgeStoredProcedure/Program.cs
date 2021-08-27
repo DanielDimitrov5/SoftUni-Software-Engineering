0e37d526a40b1e3c4bc0bd516bb77c37bfc71b77
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-08-27 14:54:21 +0300

    add ADO.NET

---
+++ C# DB/Entity Framework Core/ADO.NET/IncreaseAgeStoredProcedure/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IncreaseAgeStoredProcedure
+{
+    using System;
+    using Microsoft.Data.SqlClient;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=.;database=MinionsDB;Integrated Security=True;";
+
+            int id = int.Parse(Console.ReadLine());
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand($"EXEC usp_GetOlder {id}", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand($"SELECT Name, Age FROM Minions WHERE Id = {id}", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        Console.WriteLine($"{reader["Name"]} – {reader["Age"]} years old");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/ADO.NET/IncreaseMinionAge/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace IncreaseMinionAge
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.Data.SqlClient;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=.;database=MinionsDB;Integrated Security=True;";
+
+            int[] idInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (var id in idInput)
+                {
+                    string query = @$"UPDATE Minions
+                                        SET Age += 1,
+                                        	Name = UPPER(LEFT(Name, 1)) + 
+                                        		   LOWER(RIGHT(Name, LEN(Name) - 1))
+                                          WHERE Id = {id}";
+
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    using (var command = new SqlCommand($"SELECT Name, Age FROM Minions WHERE Id = {id}", connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            reader.Read();
+                            Console.WriteLine($"{reader[0]} {reader[1]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
