b6435d075db45705087ac6ec9047c19c3fa95e7d
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-09-07 14:42:21 +0300

    update a task

--- C# DB/Entity Framework Core/Introduction to Entity Framework/IncreaseSalaries/StartUp.cs
+++ C# DB/Entity Framework Core/Introduction to Entity Framework/IncreaseSalaries/StartUp.cs
@@ -5,7 +5,7 @@
     using System.Text;
     using SoftUni.Data;
     using SoftUni.Models;
-    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
 
     public class StartUp
     {
@@ -22,11 +22,14 @@
         {
             StringBuilder sb = new StringBuilder();
 
-            List<Employee> employees = context.Employees
-                .Where(x => x.Department.Name == "Engineering" ||
-                            x.Department.Name == "Tool Design" ||
-                            x.Department.Name == "Marketing" ||
-                            x.Department.Name == "Information Services")
+            Func<Employee, bool> departmentCheck = x => x.Department.Name == "Engineering" ||
+                                                        x.Department.Name == "Tool Design" ||
+                                                        x.Department.Name == "Marketing" ||
+                                                        x.Department.Name == "Information Services";
+
+            var employees = context.Employees
+                .Include(x => x.Department)
+                .Where(departmentCheck)
                 .OrderBy(x => x.FirstName)
                 .ThenBy(x => x.LastName)
                 .ToList();
