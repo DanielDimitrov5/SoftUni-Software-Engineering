c233fcc687c23d91d45f95f2ed27f79c801b22c9
Daniel <dani_dimitrov2003@abv.bg>
2021-09-11 09:38:51 +0300

    Update StartUp.cs

--- C# DB/Entity Framework Core/Advanced Querying/BookShopDB/BookShop/StartUp.cs
+++ C# DB/Entity Framework Core/Advanced Querying/BookShopDB/BookShop/StartUp.cs
@@ -1,12 +1,8 @@
-﻿namespace BookShop
+namespace BookShop
 {
     using System;
-    using System.Linq;
-    using System.Text;
-
     using Data;
     using Initializer;
-    using Models.Enums;
 
     public class StartUp
     {
@@ -14,36 +10,6 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
-
-            string command = Console.ReadLine();
-
-            string result = GetBooksByAgeRestriction(db, command);
-
-            Console.WriteLine(result);
-        }
-
-        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
-        {
-            AgeRestriction ageRestriction = default;
-
-            switch (command.ToLower())
-            {
-                case "minor": ageRestriction = AgeRestriction.Minor; break;
-                case "teen": ageRestriction = AgeRestriction.Teen; break;
-                case "adult": ageRestriction = AgeRestriction.Adult; break;
-
-            }
-
-            var books = context.Books.Where(x => x.AgeRestriction == ageRestriction).Select(x => new { x.Title }).OrderBy(x => x.Title);
-
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var book in books)
-            {
-                sb.AppendLine(book.Title);
-            }
-
-            return sb.ToString().TrimEnd();
         }
     }
 }
