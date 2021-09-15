4a84a1bc463eee2d9921908a7081c300816b753c
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-09-15 15:05:57 +0300

    update

--- C# DB/Entity Framework Core/Auto Mapping Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ C# DB/Entity Framework Core/Auto Mapping Objects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -1,14 +1,14 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using FastFood.Core.ViewModels.Categories;
-using FastFood.Core.ViewModels.Employees;
-using FastFood.Core.ViewModels.Items;
-using FastFood.Core.ViewModels.Orders;
-
-namespace FastFood.Core.MappingConfiguration
+﻿namespace FastFood.Core.MappingConfiguration
 {
+    using System;
+
     using AutoMapper;
     using FastFood.Models;
     using ViewModels.Positions;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Core.ViewModels.Employees;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Core.ViewModels.Orders;
 
     public class FastFoodProfile : Profile
     {
@@ -18,57 +18,40 @@ namespace FastFood.Core.MappingConfiguration
             this.CreateMap<CreatePositionInputModel, Position>()
                 .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
 
-            this.CreateMap<Position, PositionsAllViewModel>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            this.CreateMap<Position, PositionsAllViewModel>();
 
             //Categories
             this.CreateMap<CreateCategoryInputModel, Category>()
                 .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
 
-            this.CreateMap<Category, CategoryAllViewModel>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            this.CreateMap<Category, CategoryAllViewModel>();
 
             //employees
             this.CreateMap<Position, RegisterEmployeeInputModel>()
                 .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
 
             this.CreateMap<Employee, EmployeesAllViewModel>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
-                .ForMember(x => x.Age, y => y.MapFrom(s => s.Age))
-                .ForMember(x => x.Address, y => y.MapFrom(s => s.Address))
                 .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
 
-            this.CreateMap<RegisterEmployeeInputModel, Employee>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
-                .ForMember(x => x.Age, y => y.MapFrom(s => s.Age))
-                .ForMember(x => x.Address, y => y.MapFrom(s => s.Address))
-                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.PositionId));
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
 
             //Items
             this.CreateMap<Category, CreateItemViewModel>()
                 .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
 
-            this.CreateMap<CreateItemInputModel, Item>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
-                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.CategoryId))
-                .ForMember(x => x.Price, y => y.MapFrom(s => s.Price));
+            this.CreateMap<CreateItemInputModel, Item>();
 
             this.CreateMap<Item, ItemsAllViewModels>()
-                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name))
-                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name))
-                .ForMember(x => x.Price, y => y.MapFrom(s => s.Price));
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
 
-            ////Orders???
+            ////Orders
             this.CreateMap<CreateOrderInputModel, Order>()
-                .ForMember(x => x.Customer, y => y.MapFrom(s => s.Customer))
-                .ForMember(x => x.EmployeeId, y => y.MapFrom(s => s.EmployeeId));
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.Now));
 
 
             this.CreateMap<Order, OrderAllViewModel>()
                 .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
-                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
-                .ForMember(x => x.Customer, y => y.MapFrom(s => s.Customer))
-                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime));
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name));
         }
     }
 }
--- C# DB/Entity Framework Core/Auto Mapping Objects/FastFood.Core/Startup.cs
+++ C# DB/Entity Framework Core/Auto Mapping Objects/FastFood.Core/Startup.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace FastFood.Core
 {
     using Data;
+    using AutoMapper;
+    using System.Reflection;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class Startup
     {
