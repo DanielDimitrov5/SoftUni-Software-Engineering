150b091f180b267a8251db71a8d53d25859651b4
Daniel Dimitrov <dani_dimitrov2003@abv.bg>
2021-09-19 17:20:38 +0300

    add

---
+++ C# DB/Entity Framework Core/JSON Processing/Export Categories by Products Count/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+            string result = GetCategoriesByProductsCount(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var users = context.Categories
+                .Select(x => new
+                {
+                    Category = x.Name,
+                    ProductsCount = x.CategoryProducts.Count,
+                    AveragePrice = x.CategoryProducts.Average(p => p.Product.Price).ToString("F2"),
+                    TotalRevenue = x.CategoryProducts.Sum(p => p.Product.Price).ToString("F2")
+                })
+                .OrderByDescending(x => x.ProductsCount);
+
+            var resolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            var json = JsonConvert.SerializeObject(users,
+                new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented,
+                    ContractResolver = resolver
+                });
+
+            return json;
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core/JSON Processing/Export Users and Products/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+            string result = GetUsersWithProducts(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context
+                .Users
+                .AsEnumerable()
+                .Where(u => u.ProductsSold.Any(ps => ps.Buyer != null))
+                .OrderByDescending(u => u.ProductsSold.Count(ps => ps.Buyer != null))
+                .Select(u => new
+                {
+                    u.FirstName,
+                    u.LastName,
+                    u.Age,
+                    SoldProducts = new
+                    {
+                        Count = u.ProductsSold.Count(ps => ps.Buyer != null),
+                        Products = u.ProductsSold
+                            .Where(p => p.Buyer != null)
+                            .Select(p => new
+                            {
+                                p.Name,
+                                p.Price
+                            })
+                    }
+                })
+                .ToList();
+
+            var output = new
+            {
+                UsersCount = users.Count,
+                Users = users
+            };
+
+            var resolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            var json = JsonConvert.SerializeObject(output,
+                new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented,
+                    ContractResolver = resolver,
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+
+            return json;
+        }
+    }
+}
